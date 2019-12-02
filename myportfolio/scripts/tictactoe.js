0e015d0653604855a831357574ac209add731afe
Ernesto-Sanchez-J <ernesto.j.sanchez12@gmail.com>
2019-12-01 18:29:12 -0600

    is it working

---
+++ myportfolio/scripts/tictactoe.js
@@ -0,0 +1,57 @@
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+];
+
+const grid = () => Array.from(document.getElementsByClassName('q'));
+const qNumid = (qE1) => Number.parseInt(qE1.id.replace('q', ''));
+const emptyQs = () => grid().filter(_qE1 => _qE1.innerText === '');
+const allSame = (arr) => Array.every(_qE1 => _qE1.innerText === arr[0].innerText && _qE1.innerText !=== '');
+
+const takeTurn = (index, letter) => grid()[index].innerText = letter;
+const opponentChoice = () => qNumid(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
+
+const endGame = () => { 
+    winningSequence.forEach(_qE1 => _qE1.classlist.add('winner'));
+    disableListeners();
+ }
+
+
+const checkForVictory = () => {
+    let victory = false;
+    winningCombos.forEach(_c => {
+        const _grid =grid();
+        const sequence = [_grid[_c[0]], _grid[_c[1]], _grid[_c[2]]];
+        if(allSame(sequence)) {
+            victory = true;
+            endGame(sequence);
+        }
+    });
+}
+
+const opponentTurn = () => {
+    disableListeners();
+    setTimeout(() => {
+        takeTurn(opponentChoice(), 'o');
+        if(!checkForVictory())
+        enableListeners();
+    }, 1000);
+}
+
+const clickFn = (event) => {
+    takeTurn(qNumid(event.target), 'x');
+    if(!checkForVictory())
+    opponentTurn();
+
+};
+
+const enableListeners = () => grid().forEach(_qE1.addEventListener('click',clickFn));
+const disableListeners = () => grid().forEach(_qE1 => _qE1.removeEventListeners('click', clickFn));
+
+enableListeners();
