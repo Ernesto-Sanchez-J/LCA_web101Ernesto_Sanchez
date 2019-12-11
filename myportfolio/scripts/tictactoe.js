660f10358bfa808a10f5fe4b8fb345cde45f8745
Ernesto-Sanchez-J <ernesto.j.sanchez12@gmail.com>
2019-12-10 19:38:11 -0600

    dash calendar

--- myportfolio/scripts/tictactoe.js
+++ myportfolio/scripts/tictactoe.js
@@ -9,13 +9,25 @@ const winningCombos = [
     [2,5,8],
 ];
 
-const grid = () => Array.from(document.getElementsByClassName('q'));
-const qNumid = (qE1) => Number.parseInt(qE1.id.replace('q', ''));
-const emptyQs = () => grid().filter(_qE1 => _qE1.innerText === '');
-const allSame = (arr) => Array.every(_qE1 => _qE1.innerText === arr[0].innerText && _qE1.innerText !=== '');
+const grid = () => {
+    return Array.from(document.getElementsByClassName('q'));
+};
+const qNumid = (qE1) => {
+    return Number.parseInt(qE1.id.replace('q', ''));
+};
+const emptyQs = () => {
+    return grid().filter(_qE1 => _qE1.innerText === '');
+};
+const allSame = (arr) => {
+    return Array.every(_qE1 => _qE1.innerText === arr[0].innerText && _qE1.innerText != '');
+};
 
-const takeTurn = (index, letter) => grid()[index].innerText = letter;
-const opponentChoice = () => qNumid(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
+const takeTurn = (index, letter) => {
+    return grid()[index].innerText = letter;
+};
+const opponentChoice = () => {
+    return qNumid(emptyQs()[Math.floor(Math.random() * emptyQs().length)]);
+};
 
 const endGame = () => { 
     winningSequence.forEach(_qE1 => _qE1.classlist.add('winner'));
